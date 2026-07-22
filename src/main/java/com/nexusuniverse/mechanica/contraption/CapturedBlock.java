fbf41a15de1542b23fd9d5cafd24c082b257ea14
mrlion101az-cell <mrlion101az@gmail.com>
2026-07-22 13:30:34 -0700

    Delete src/main directory

--- src/main/java/com/nexusuniverse/mechanica/contraption/CapturedBlock.java
+++
@@ -1,7 +0,0 @@
-package com.nexusuniverse.mechanica.contraption;
-
-import org.bukkit.block.data.BlockData;
-
-/** One block captured into a contraption, as an offset from the anchor and its original BlockData. */
-public record CapturedBlock(int relX, int relY, int relZ, BlockData blockData) {
-}
